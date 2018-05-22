2a0877f126a8980ff85d2177e403aab75eaae130
CzaplewskiC2018 <chuckczaplewski@gmail.com>
2018-05-22 09:23:57 -0500

    index.html

---
+++ main.js
@@ -0,0 +1,14 @@
+alert("You are now entering silly times!");
+
+var noun1 = prompt("please enter a noun.", "");
+var verb1 = prompt("please enter a verb.", "");
+var noun2 = prompt("please enter a noun.", "");
+var verb2 = prompt("please enter a verb.", "");
+var noun4 = prompt("please enter a noun.", "");
+var adj1 = prompt("please enter an adjcive.", "");
+var verb3 = prompt("please enter a verb.", "");
+var verb4 = prompt("please enter a verb.", "");
+var adj2 = prompt("please enter an adjcive.", "");
+var noun4 = prompt("please enter a noun.", "");
+
+document.getElementById("name").innerHTML = "the " + noun1 + "ran to the " + verb1 + " he then went to the " + noun2 + " . He later went to see the " + verb2 + " he then " + adj1 + "in the face" + verb3 + " this later led them to " + verb4 + " he then bulled out a " + adj2 + " he then proceded to. The end! " 
